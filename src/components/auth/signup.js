70af941b598cbef90020db6310864fc5427d9f05
stoneprogrammer <stoneprogrammer@gmail.com>
2019-12-20 13:21:46 -0700

    developed sign up form and modified form button

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 
+import SignUpForm from './signupForm'
+import PageTitle from '../pageTitle'
+
 class SignUp extends Component {
+
+  onSubmit = fields => {
+    console.log(fields)
+  }
+
   render() {
     return (
       <div className='sign-up'>
-        sign up
+        <PageTitle className='sign-up__page-title' title='Register' />
+        <SignUpForm onSubmit={this.onSubmit} className='sign-in__form' />
       </div>
     )
   }
 }
+
 
 export default SignUp
