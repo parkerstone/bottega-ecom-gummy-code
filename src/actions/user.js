6bcf69e4d9f17f20c943b4cf7a01c27da98662b2
stoneprogrammer <stoneprogrammer@gmail.com>
2019-12-20 20:32:35 -0700

    finished purchases component

--- src/actions/user.js
+++ src/actions/user.js
@@ -8,13 +8,91 @@ export function fetchUserPurchases() {
     payload: [
       {
         _id: 0,
-        title: 'purchase 1',
-        amount: 8.02
+        total: 8.02,
+        orderNumber: 'A0048248343',
+        orderDate: new Date(),
+        creditCard: '-0000',
+        user: {
+          name: 'Jordan Hudgens',
+          shippingAddress: '1234 West State Street'
+        }
+      },
+      {
+        _id: 1,
+        total: 1.30,
+        orderNumber: 'a54sdf54wef',
+        orderDate: new Date(),
+        creditCard: '-0000',
+        user: {
+          name: 'Parker Stone',
+          shippingAddress: '4321 West State Street'
+        }
       },
       {
         _id: 2,
-        title: 'purchase 2',
-        amount: 19.40
+        total: 88.02,
+        orderNumber: 'b0048248343',
+        orderDate: new Date(),
+        creditCard: '-0000',
+        user: {
+          name: 'Jordan Hudgens',
+          shippingAddress: '1234 West State Street'
+        }
+      },
+      {
+        _id: 3,
+        total: 8.02,
+        orderNumber: 'A0048248343',
+        orderDate: new Date(),
+        creditCard: '-0000',
+        user: {
+          name: 'Jordan Hudgens',
+          shippingAddress: '1234 West State Street'
+        }
+      },
+      {
+        _id: 4,
+        total: 8.02,
+        orderNumber: 'A0048248343',
+        orderDate: new Date(),
+        creditCard: '-0000',
+        user: {
+          name: 'Jordan Hudgens',
+          shippingAddress: '1234 West State Street'
+        }
+      },
+      {
+        _id: 5,
+        total: 8.02,
+        orderNumber: 'A0048248343',
+        orderDate: new Date(),
+        creditCard: '-0000',
+        user: {
+          name: 'Jordan Hudgens',
+          shippingAddress: '1234 West State Street'
+        }
+      },
+      {
+        _id: 6,
+        total: 8.02,
+        orderNumber: 'A0048248343',
+        orderDate: new Date(),
+        creditCard: '-0000',
+        user: {
+          name: 'Jordan Hudgens',
+          shippingAddress: '1234 West State Street'
+        }
+      },
+      {
+        _id: 7,
+        total: 8.02,
+        orderNumber: 'A0048248343',
+        orderDate: new Date(),
+        creditCard: '-0000',
+        user: {
+          name: 'Jordan Hudgens',
+          shippingAddress: '1234 West State Street'
+        }
       }
     ]
   })
--- src/components/account/purchaseHistory.js
+++ src/components/account/purchaseHistory.js
@@ -9,8 +9,10 @@ class PurchaseHistory extends Component {
     return (
       <div className='purchase-history'>
         <PageTitle className='purchase-history__page-title' title='Purchase History' />
-        <Purchases className='purchas-history__purchases' />
-        <PurchaseDetail className='purchase-hisory__detail' />
+        <div className="purchase-history__content">
+          <Purchases className='purchas-history__purchases' />
+          <PurchaseDetail className='purchase-hisory__detail' />
+        </div>
         <div className="purchase-history__border-bottom"></div>
       </div>
     )
