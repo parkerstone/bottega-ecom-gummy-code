d55ed954345ce2d66a6773733987e5ecfdfb72cd
stoneprogrammer <stoneprogrammer@gmail.com>
2019-12-21 19:41:08 -0700

    finished account information form

--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -65,30 +65,48 @@ class AccountInformationForm extends Component {
           name='zipcode'
           component={FormInput}
         />
+        <div className="account-information-form__line" key={3}></div>
         {this.state.showPasswords ? ([
           <Field key={0}
-          className='account-information-form__current'
-          type='password'
-          title='Current Password'
-          placeholder='Current Password'
-          name='current'
-          component={FormInput}
+            className='account-information-form__current'
+            type='password'
+            title='Current Password'
+            placeholder='Current Password'
+            name='current'
+            component={FormInput}
           />,
           <Field key={1}
-          className='account-information-form__new'
-          type='password'
-          title='New Password'
-          placeholder='New Password'
-          name='new'
-          component={FormInput}
+            className='account-information-form__new'
+            type='password'
+            title='New Password'
+            placeholder='New Password'
+            name='new'
+            component={FormInput}
           />,
           <Field key={2}
-          className='account-information-form__confirm'
-          type='password'
-          title='Confirm Password'
-          placeholder='Confirm Password'
-          name='confirm'
-          component={FormInput}
+            className='account-information-form__confirm'
+            type='password'
+            title='Confirm Password'
+            placeholder='Confirm Password'
+            name='confirm'
+            component={FormInput}
+          />,
+          <Field key={3}
+            className='account-information-form__update-information'
+            onClick={() => this.setState({ showPasswords: false })}
+            type='submit'
+            title='Update Information'
+            name='update-information'
+            component={FormButton}
+          />,
+          <Field key={4}
+            className='account-information-form__cancel'
+            onClick={() => this.setState({ showPasswords: false })}
+            type='button'
+            title='Cancel'
+            name='cancel'
+            short={true}
+            component={FormButton}
           />
         ]) : (
         <Field
