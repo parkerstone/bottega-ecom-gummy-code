8cc54bdb5a1b1a23f4d12e2dd72454303206513f
stoneprogrammer <stoneprogrammer@gmail.com>
2019-12-21 22:59:13 -0700

    filter products with query action creator

--- src/actions/shop.js
+++ src/actions/shop.js
@@ -2,8 +2,16 @@ import {
   SET_SHOP_CATEGORIES,
   SET_NAVBAR_LINKS,
   SET_SHOP_PRODUCTS,
-  FILTER_PRODUCTS_WITH_CATEGORY_ID
+  FILTER_PRODUCTS_WITH_CATEGORY_ID,
+  FILTER_PRODUCTS_WITH_QUERY
 } from './types';
+
+export function filterProductsWithQuery(fields) {
+  return ({
+    type: FILTER_PRODUCTS_WITH_QUERY,
+    payload: fields
+  })
+}
 
 export function filterProductsWithCategoryId(_id) {
   return ({
@@ -76,7 +84,7 @@ export function fetchShopProducts() {
       },
       {
         _id: 3,
-        title: 'Use Interface Design',
+        title: 'User Interface Design',
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non, repellat ex sequi aliquid voluptatem voluptatibus dolores praesentium, nobis obcaecati nemo sint. Illum, provident! Repudiandae tempora, sed est iure cumque quibusdam.',
         price: 1.99,
         belongsTo: [0, 2]
--- src/reducers/shopReducer.js
+++ src/reducers/shopReducer.js
@@ -1,7 +1,8 @@
 import {
   SET_SHOP_CATEGORIES,
   SET_SHOP_PRODUCTS,
-  FILTER_PRODUCTS_WITH_CATEGORY_ID
+  FILTER_PRODUCTS_WITH_CATEGORY_ID,
+  FILTER_PRODUCTS_WITH_QUERY
 } from '../actions/types';
 
 const INITIAL_STATE = {
@@ -27,6 +28,17 @@ export default function(state=INITIAL_STATE, action) {
       var filteredProducts = [];
       state.products.forEach(product => {
         if(product.belongsTo.includes(action.payload)) {
+          filteredProducts.push(product);
+        }
+      })
+      return {
+        ...state,
+        filteredProducts
+      }
+    case FILTER_PRODUCTS_WITH_QUERY:
+      var filteredProducts = [];
+      state.products.forEach(product => {
+        if(product.title.toLowerCase().includes(action.payload.query.toLowerCase())) {
           filteredProducts.push(product);
         }
       })
