0d4ed3e9189f4aeee70bb8a7a721295188ef961b
stoneprogrammer <stoneprogrammer@gmail.com>
2019-12-23 23:23:25 -0700

    added and styled missing shop cart component

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import ShopSearchBar from './shopSearchBar';
 import ShopProduct from './shopProduct';
+import ShopCart from './shopCart';
 
 class Shop extends Component {
 
@@ -35,6 +36,7 @@ class Shop extends Component {
   render() {
     return (
       <div className='shop'>
+        <ShopCart />
         <ShopSearchBar onSubmit={this.onSubmit} className='shop__search-bar' />
         <div className="shop__products">
           {
