7b4f368194a4a3ad730fe24345b1933aae9c4b82
stoneprogrammer <stoneprogrammer@gmail.com>
2019-12-27 15:36:18 -0700

    review form grid

--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -14,7 +14,7 @@ class ReviewForm extends Component {
         <div className="review-form__line"></div>
         <Field
           className='review-form__proceed'
-          onClick={() => history.push('/account')}
+          onClick={() => history.push('/signin')}
           type='submit'
           title='Proceed to Checkout'
           name='proceed'
@@ -22,7 +22,7 @@ class ReviewForm extends Component {
         />
         <Field
           className='review-form__back'
-          onClick={() => history.push('/signin')}
+          onClick={() => history.push('/shop')}
           type='button'
           title='Back'
           name='back'
