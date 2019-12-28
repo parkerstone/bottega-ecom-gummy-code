d171ace11df02c30f6fb2d753c47f98ad4f76774
stoneprogrammer <stoneprogrammer@gmail.com>
2019-12-27 22:17:14 -0700

    built orderSummary and infoHelp

--- src/components/information/shippingForm.js
+++ src/components/information/shippingForm.js
@@ -5,6 +5,8 @@ import { FormInput, FormButton } from '../formFields';
 
 import history from '../../history';
 
+import OrderSummary from './orderSummary';
+
 class ShippingForm extends Component {
   render() {
     const { className, handleSubmit } = this.props;
@@ -69,7 +71,7 @@ class ShippingForm extends Component {
           short={true}
           component={FormButton}
         />
-
+        <OrderSummary className='shipping-form__summary' />
       </form>
     )
   }
