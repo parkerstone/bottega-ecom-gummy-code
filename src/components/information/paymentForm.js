01b1a1f3f9d69bc30a6774da4b2a81a5fbbeb96b
stoneprogrammer <stoneprogrammer@gmail.com>
2019-12-27 23:58:27 -0700

    built payment form

--- src/components/information/paymentForm.js
+++ src/components/information/paymentForm.js
@@ -1,74 +1,84 @@
 import React, { Component } from 'react';
+
 import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux'; 
 
 import { FormInput, FormButton } from '../formFields';
 
 import history from '../../history';
+import OrderSummary from './orderSummary';
+import { UnderlinedTitle } from './infoHelp';
 
 class PaymentForm extends Component {
   render() {
-    const { className, handleSubmit } = this.props;
+      const { className, handleSubmit } = this.props;
 
-    return (
-      <form className={`${className} sign-up-form`} onSubmit={handleSubmit}>
-        <Field
-          className='sign-up-form__name'
-          type='name'
-          title='Name'
-          placeholder='Name'
-          name='name'
-          component={FormInput}
-        />
-        <Field
-          className='sign-up-form__email'
-          type='email'
-          title='Email'
-          placeholder='Email'
-          name='email'
-          component={FormInput}
-        />
-        <Field
-          className='sign-up-form__password'
-          type='password'
-          title='Password'
-          placeholder='Password'
-          name='password'
-          component={FormInput}
-        />
-        <Field
-          className='sign-up-form__confirm'
-          type='password'
-          title='Confirm Password'
-          placeholder='Confirm Password'
-          name='confrim'
-          component={FormInput}
-        />
-        <div className="sign-up-form__line"></div>
-        <Field
-          className='sign-up-form__login'
-          onClick={() => history.push('/account')}
-          type='submit'
-          title='Create Account'
-          name='login'
-          component={FormButton}
-        />
-        <Field
-          className='sign-up-form__back'
-          onClick={() => history.push('/signin')}
-          type='button'
-          title='Back'
-          name='back'
-          short={true}
-          component={FormButton}
-        />
-
-      </form>
+      return (
+        <form onSubmit={handleSubmit} className={`${className} payment-form`}>
+          <Field className='payment-form__name'
+            type='name'
+            title='Name on Credit Card'
+            placeholder='Name'
+            name='name'
+            component={FormInput}
+          />
+          <Field className='payment-form__card'
+            type='card'
+            title='Credit Card Number'
+            placeholder='____-____-____-____'
+            name='card'
+            component={FormInput}
+          />
+          <Field className='payment-form__expiration'
+            type='expiration'
+            title='Expiration Date'
+            placeholder='expiration'
+            name='expiration'
+            component={FormInput}
+          />
+          <Field className='payment-form__ccv'
+            type='ccv'
+            title='CCV'
+            placeholder='CCV'
+            name='ccv'
+            component={FormInput}
+          />
+          <div className='payment-form__line'></div>
+          <Field className='payment-form__pay-complete'
+            onClick={() => history.push('/information/payment')}
+            type='submit'
+            title='Pay & Complete'
+            name='pay-complete'
+            component={FormButton}
+          />
+          <Field className='payment-form__back'
+            onClick={() => history.push('/signin')}
+            type='button'
+            title='Back'
+            name='back'
+            short={true}
+            component={FormButton}
+          />
+          <OrderSummary className='payment-form__order-summary'/>
+          <div className='payment-form__shipping-info shipping-info'>
+            <UnderlinedTitle className='shipping-info__title' title='Shipping To'/>
+            <div className='shipping-info__name small-text'>{this.props.name}</div>
+            <div className='shipping-info__address small-text'>{this.props.address}</div>
+          </div>
+        </form>
     )
   }
 }
 
 PaymentForm = reduxForm({
   form: 'PaymentForm'
-})(PaymentForm)
+})(PaymentForm);
+
+function mapStateToProps(state) {
+  const { name, address } = state.user.user;
+  return { name, address }
+}
+
+// PaymentForm = connect(mapStateToProps)(PaymentForm);
 
 export default PaymentForm;
